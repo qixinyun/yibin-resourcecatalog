be2c769e8b8077b74cde1e5dc1a886e6ada14b69
Leon <apple@apples-MacBook-Pro.local>
2019-03-25 14:51:42 +0800

    init resourcecatalog

--- src/ResourceCatalog/Model/IResourceCatalogTypeEnName.php
+++ src/ResourceCatalog/Model/IResourceCatalogTypeEnName.php
@@ -4,7 +4,7 @@ namespace ResourceCatalog\Model;
 interface IResourceCatalogTypeEnName
 {
     const TYPE_ENNAME = array(
-        IResourceCatalog::TYPE['NULL'] => 0,
+        IResourceCatalog::TYPE['NULL'] => '',
         IResourceCatalog::TYPE['XBXZCF'] => 'xbxzcf',
         IResourceCatalog::TYPE['XBXZXK'] => 'xbxzxk',
         IResourceCatalog::TYPE['TEST'] => 'test'
--- src/ResourceCatalog/Model/IResourceCatalogTypeIdentify.php
+++ src/ResourceCatalog/Model/IResourceCatalogTypeIdentify.php
@@ -4,7 +4,7 @@ namespace ResourceCatalog\Model;
 interface IResourceCatalogTypeIdentify
 {
     const TYPE_IDENTIFY = array(
-        IResourceCatalog::TYPE['NULL'] => 0,
+        IResourceCatalog::TYPE['NULL'] => '',
         IResourceCatalog::TYPE['XBXZCF'] => 'XBXZCF',
         IResourceCatalog::TYPE['XBXZXK'] => 'XBXZXK',
         IResourceCatalog::TYPE['TEST'] => 'TEST'
